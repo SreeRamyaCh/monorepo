683f17badfb33949ffde0992e1fbbb8c21e4aa2b
SreeRamya Ch <sreeramya.gudipati@gmail.com>
2021-11-11 00:31:03 +0530

    Created a basic Express JS Server

--- server/server.js
+++ server/server.js
@@ -2,3 +2,9 @@
 const express = require("express");
 //Initialize a server side app.
 const app = express();
+//Define a port address.
+const port = 5000;
+//Listen the app on port.
+app.listen(port, () =>{
+  console.log(`Server started in port ${port}.`)
+})
