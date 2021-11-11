d4bbceb62fb265c8e214a757c62d5f2328b3fb0d
SreeRamya Ch <sreeramya.gudipati@gmail.com>
2021-11-11 21:18:48 +0530

    updated custom middleware using Express BodyParser

--- server/server.js
+++ server/server.js
@@ -5,14 +5,22 @@ const app = express();
 //Define a port address.
 const port = 5000;
 
+//Use a Middleware to parse POST Data.
+/* app.use((req, res, next) => {
+  console.log({ req, res });
+  next();
+  console.log({ req, res });
+}); */
+app.use(express.json());
+
 //Create a GET Path.
-app.get("/", (req, res) =>{
+app.get("/", (req, res) => {
   res.send("Hello Geeks");
 });
-app.post("/", (req, res) =>{
+app.post("/", (req, res) => {
   res.send("Hello Ramya");
 });
 //Listen the app on port.
-app.listen(port, () =>{
-  console.log(`Server started in port ${port}.`)
+app.listen(port, () => {
+  console.log(`Server started in port ${port}.`);
 });
