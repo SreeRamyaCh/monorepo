e5fc8656cc8a30a2c0f3b068e6e933552e10e218
SreeRamya Ch <sreeramya.gudipati@gmail.com>
2021-11-11 23:36:24 +0530

    Dynamic Parameter Routing

--- server/server.js
+++ server/server.js
@@ -11,11 +11,15 @@ app.use(express.urlencoded({ extended: true }));
 
 //Create a GET Path.
 app.get("/", (req, res) => {
-  let output = "Hello Geeks";
+  let output = "Hello Geeks, Welcome to HomePage!";
   if (Object.values(req.query).length > 0) {
     output += "Here's some queries:" + JSON.stringify(req.query, null, 2);
   }
   res.send(output);
+});
+//Dynamic Parameter Routing.
+app.get("/:user", (req, res) => {
+  res.send(`Hello Geeks, Welcome to ${req.params.user}'s!`);
 });
 app.post("/", (req, res) => {
   let output = "Hello Ramya!!";
