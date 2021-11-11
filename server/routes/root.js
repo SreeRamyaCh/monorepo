8933a497e35e32d7bc5ac9261bac30eb78988296
SreeRamya Ch <sreeramya.gudipati@gmail.com>
2021-11-11 23:50:14 +0530

    Implemented Route for root

--- server/routes/root.js
+++ server/routes/root.js
@@ -4,5 +4,28 @@ const express = require("express");
 //Create an instance of router.
 const app = express.Router();
 
+//Create a GET Path.
+app.get("/", (req, res) => {
+  let output = "Hello Geeks, Welcome to HomePage!";
+  if (Object.values(req.query).length > 0) {
+    output += "Here's some queries:" + JSON.stringify(req.query, null, 2);
+  }
+  res.send(output);
+});
+//Dynamic Parameter Routing.
+app.get("/:user", (req, res) => {
+  res.send(`Hello Geeks, Welcome to ${req.params.user}'s!`);
+});
+app.post("/", (req, res) => {
+  let output = "Hello Ramya!!";
+  if (Object.values(req.query).length > 0) {
+    output += "Here's some queries:" + JSON.stringify(req.query, null, 2);
+  }
+  if (Object.values(req.body).length > 0) {
+    output += "Here's some body:" + JSON.stringify(req.body, null, 2);
+  }
+  res.send(output);
+});
+
 //Export the app.
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -4,33 +4,16 @@ const express = require("express");
 const app = express();
 //Define a port address.
 const port = 5000;
+//Import Routes.
+const root = require("./routes/root");
 
 //Use a Middleware to parse POST Data.
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Create a GET Path.
-app.get("/", (req, res) => {
-  let output = "Hello Geeks, Welcome to HomePage!";
-  if (Object.values(req.query).length > 0) {
-    output += "Here's some queries:" + JSON.stringify(req.query, null, 2);
-  }
-  res.send(output);
-});
-//Dynamic Parameter Routing.
-app.get("/:user", (req, res) => {
-  res.send(`Hello Geeks, Welcome to ${req.params.user}'s!`);
-});
-app.post("/", (req, res) => {
-  let output = "Hello Ramya!!";
-  if (Object.values(req.query).length > 0) {
-    output += "Here's some queries:" + JSON.stringify(req.query, null, 2);
-  }
-  if (Object.values(req.body).length > 0) {
-    output += "Here's some body:" + JSON.stringify(req.body, null, 2);
-  }
-  res.send(output);
-});
+//Require Modular Routes.
+app.use("/", root);
+
 //Listen the app on port.
 app.listen(port, () => {
   console.log(`Server started in port ${port}.`);
